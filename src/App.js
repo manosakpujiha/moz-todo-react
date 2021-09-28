7d1ac42666eda3b0edcc6bab2c1bbfb50def68aa
manos <uwmano@yahoo.com>
2021-09-28 17:19:23 +0100

    filters, and edit

--- src/App.js
+++ src/App.js
@@ -4,11 +4,30 @@ import Form from "./components/Form";
 import React, { useState } from "react";
 import {nanoid} from "nanoid";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState('All');
   function addTask(name) {
     const newTask = {id: "todo-" + nanoid(), name: name, completed: false};
     setTasks([...tasks, newTask]);
+  }
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map(task => {
+    // if this task has the same ID as the edited task
+      if (id === task.id) {
+        //
+        return {...task, name: newName}
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
   }
   function toggleTaskCompleted(id) {
     const updatedTasks = tasks.map(task => {
@@ -27,14 +46,25 @@ function App(props) {
     setTasks(remainingTasks);
   }
   
-  const taskList = tasks.map(task => (
-    <Todo
-      id={task.id}
-      name={task.name}
-      completed={task.completed}
-      key={task.id}
-      toggleTaskCompleted={toggleTaskCompleted}
-      deleteTask={deleteTask}
+  const taskList = tasks
+.filter(FILTER_MAP[filter])
+.map(task => (
+  <Todo
+    id={task.id}
+    name={task.name}
+    completed={task.completed}
+    key={task.id}
+    toggleTaskCompleted={toggleTaskCompleted}
+    deleteTask={deleteTask}
+    editTask={editTask}
+  />
+));
+  const filterList = FILTER_NAMES.map(name => (
+    <Filter
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
     />
   ));
   const headingText = `${taskList.length} tasks remaining`;
@@ -43,9 +73,7 @@ function App(props) {
       <h1>TodoMatic</h1>
         <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
-        <Filter/> 
-        <Filter/>
-        <Filter/>
+      {filterList}
       </div>
 
       <h2 id = "list-heading">
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,11 +1,17 @@
-const Filter = () => {
+const Filter = (props) => {
+
     return (
        
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show </span>
-          <span>all</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
+      <button
+      type="button"
+      className="btn toggle-btn"
+      aria-pressed={props.isPressed}
+      onClick={() => props.setFilter(props.name)}
+      >
+      <span className="visually-hidden">Show </span>
+      <span>{props.name}</span>
+      <span className="visually-hidden"> tasks</span>
+      </button>
         
     )
 }
